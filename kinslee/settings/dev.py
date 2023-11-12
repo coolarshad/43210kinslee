3f0d0f61544667f1b13b7eca274678486c3ae5ba
Arshad Qadir <coolarshad@Arshads-Mini.domain.name>
2023-11-12 19:57:15 +0545

    first setup

---
+++ kinslee/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q=$*&ps5o^_kd1bv6^iu9j&hs2u%^lqhb8rjmgw(k5&%3#@k51"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
